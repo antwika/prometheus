db66b391f1670696cc85091896d0cef2f2622859
Anton Wiklund <ixuz07@gmail.com>
2022-04-28 23:05:05 +0200

    fix: enforce all classes in 'src/' to be tested for coverage report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    './src': {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
